0f97b9de8bea48dea110afbe8691b48ccd7f53ae
thalesmf05 <thalesferrari5@gmail.com>
2025-12-01 15:50:23 +0000

    Implement form validation and enhance campaign submission page; update content and images for education challenges

---
+++ script/campaignForm.js
@@ -0,0 +1,138 @@
+// Form validation script]
+// Authour: Thales Ferrari
+//source https://youtu.be/CYlNJpltjMM?si=lw17KHzxEGv7bjvU
+const form = document.getElementById('form');
+const inputFirstName = document.getElementById('inputFirstName');
+const inputSurname = document.getElementById('inputSurname');
+const inputEmail = document.getElementById('inputEmail');
+const inputPhone = document.getElementById('inputPhone');
+const inputComments = document.getElementById('inputTextArea');
+const contactYes = document.getElementById('contactYes');
+const contactNo = document.getElementById('contactNo');
+const checkboxEmail = document.getElementById('checkboxEmail');
+const checkboxPhone = document.getElementById('checkboxPhone');
+const contactError = document.getElementById('contactError');
+const optionError = document.getElementById('optionError');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    validateInputs();
+});
+
+form.addEventListener('reset', event => {
+    resetErrors();
+});
+
+const resetErrors = () => {
+    const inputControls = document.querySelectorAll('.input-control-t'); //select all input control divs
+
+    inputControls.forEach(inputControl => {
+        inputControl.classList.remove('error'); //remove error class from all input control divs
+        inputControl.classList.remove('success'); //remove success class from all input control divs
+        const errorDisplay = inputControl.querySelector('.form-error-t'); //select error message empty div
+        errorDisplay.innerText = ''; //clear error message
+    });
+}
+const setError = (element, message) => { //set error message
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.form-error-t'); //select error message empty div
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error'); //add error class to the parent div
+    inputControl.classList.remove('success'); //remove success class from parent div
+}
+
+const setSucces = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".form-error-t");
+
+    errorDisplay.innerText = "";
+    inputControl.classList.add("success");
+    inputControl.classList.remove("error");
+}
+
+const isValidEmail = email => { //check if the email has the correct format
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateEmail = email => { //email validation
+    if(email === ""){ //check if email is empty
+        return setError(inputEmail, "Email is required");
+    }else if(!isValidEmail(email)){ //calls isValidEmail function to check email format
+        return setError(inputEmail, "Provide a valid email address");
+    }else{
+        return setSucces(inputEmail);
+    }
+}
+
+const validatePhone = phone => { //phone validation
+    if(phone === ""){ //check if phone is empty
+        return setError(inputPhone, "Phone number is required");   
+    }else if (phone.length < 7 || phone.length > 15){ //check if phone number length is valid
+        return setError(inputPhone, "Phone number must be between 7 and 15 digits");
+    }else{
+        return setSucces(inputPhone);
+    }
+}
+
+
+const validateInputs = () => {
+    const firstNameValue = inputFirstName.value.trim();
+    const surnameValue = inputSurname.value.trim();
+    const emailValue = inputEmail.value.trim();
+    const phoneValue = inputPhone.value.trim();
+    const commentsValue = inputComments.value.trim();
+
+    if(firstNameValue === "" || firstNameValue.length < 2){ //check if name is empty or less than 2 characters
+        setError(inputFirstName, "User name is required");
+    }else{
+        setSucces(inputFirstName); //set success if name is valid
+    }
+    
+    if(surnameValue === "" || surnameValue.length < 2){ //check if surname is empty or less than 2 characters
+        setError(inputSurname, "Surname is required");
+    }else{
+        setSucces(inputSurname); //set success if surname is valid
+    }
+
+    if (!contactYes.checked && !contactNo.checked){ //verify if user selected yes or no for contact preference, if neither is selected, show error
+        setError(contactError, "Please select an option");
+    }else if(contactYes.checked){ //only requires email and phone if user wants to be contacted
+        setSucces(contactError);
+        if(checkboxEmail.checked || checkboxPhone.checked){ //at least one contact method must be selected
+            setSucces(optionError);
+            setSucces(contactError);
+            //if email contact method is selected, validate email
+            if(checkboxEmail.checked){ //validate email if email contact method is selected
+                validateEmail(emailValue);
+            }else{ //if email contact method is not selected, set success
+                setSucces(inputEmail);
+            }
+            //if phone contact method is selected, validate phone
+            if(checkboxPhone.checked){//validate phone if phone contact method is selected
+                validatePhone(phoneValue);
+            }else{ //if phone contact method is not selected, set success
+                setSucces(inputPhone);
+            }
+        }else{ //tell user to select at least one contact method if contactYes is checked but no method is selected
+            setError(optionError, "Please select at least one contact method");
+            setError(inputEmail, ""); 
+            setError(inputPhone, "");
+        }
+    }else{ //if user does not want to be contacted, no need to validate email and phone
+        setSucces(optionError);
+        setSucces(contactError);
+        setSucces(inputEmail);
+        setSucces(inputPhone);
+    }
+
+    if(commentsValue === ""){ //validate comments
+        setError(inputComments, "Comments are required");
+    }else{
+        setSucces(inputComments);
+    }
+
+};
+
